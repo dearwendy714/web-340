efd7e24717bc1baade6f95b3fdb41cc34488348e
dearwendy <dearwendy714@gmail.com>
2019-08-12 10:45:09 -0700

    Complete assignment

---
+++ Week-1/Portillo-exercise-1.4.js
@@ -0,0 +1,20 @@
+
+/*
+============================================
+; Title:  Portillo-exercise-1.4
+; Author: Professor Krasso
+; Date:   11 August 2019
+; Modified By: Wendy Portillo 
+; Description:  Assignment 1.4
+;===========================================
+*/
+
+//Header
+
+var header = require('../header.js');
+console.log(header.display("Wendy", "Portillo", "Exercise 1.4"));
+console.log('\n');
+
+
+// Here is the console.log in an actual .js file
+console.log('You are now in Portillo \'s world!');
---
+++ Week-1/Portillo-server.js
@@ -0,0 +1,43 @@
+/*
+============================================
+; Title:  Portillo-server.js
+; Author: Professor Krasso
+; Date:   11 August 2019
+; Modified By: Wendy Portillo 
+; Description: creating my first server
+;===========================================
+*/
+
+//Header
+
+var header = require('../header.js');
+console.log(header.display("Wendy", "Portillo", "Exercise 1.3"));
+console.log('\n');
+
+
+//Variable declaration:
+
+var http = require("http");
+
+function processRequest(req, res) {
+
+// server body message
+var body = "Hello! And welcome to my very first NodeJs server, created for WEB-340.";
+
+    var contentLength = body.length;
+
+    res.writeHead(200, {
+
+        'Content-Length': contentLength,
+
+        'Content-Type': 'text/plain'
+
+    });
+
+    res.end(body);
+
+}
+
+var s = http.createServer(processRequest);
+
+s.listen(8080)
